7ca396ac24c4e2bcfdb954435a5534c344c6ad88
Amir Kumalov <akumalov@integra-soft.ru>
2022-12-05 16:40:15 +0300

    Add logout and exceptionHandling configuration

--- src/main/java/com/crynet/springsecuritydemo/configuration/SecurityConfiguration.java
+++ src/main/java/com/crynet/springsecuritydemo/configuration/SecurityConfiguration.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity(debug = true)
@@ -29,6 +31,12 @@ public class SecurityConfiguration {
                     response.getWriter().print(objectMapper.writeValueAsString(authentication.getPrincipal()));
                 })
                 .failureHandler((request, response, exception) -> response.setStatus(HttpServletResponse.SC_BAD_REQUEST));
+        http.logout()
+                .logoutUrl("/api/logout")
+                .logoutSuccessHandler((request, response, authentication) -> response.setStatus(HttpServletResponse.SC_OK))
+                .deleteCookies("QBASIC_SESSION_ID");
+        http.exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
         return http.build();
     }
 
