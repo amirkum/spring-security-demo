f14841948a305df43cff19a6d74a84eb36d6da4e
Amir Kumalov <akumalov@integra-soft.ru>
2022-12-05 15:41:51 +0300

    Add authentication with application/json data

--- src/main/java/com/crynet/springsecuritydemo/security/CustomAuthenticationFilter.java
+++ src/main/java/com/crynet/springsecuritydemo/security/CustomAuthenticationFilter.java
@@ -1,16 +1,59 @@
 package com.crynet.springsecuritydemo.security;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    private boolean postOnly = true;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        if (this.postOnly && !request.getMethod().equals("POST")) {
+            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+        }
+        Map<String, String> data;
+        try {
+            data = objectMapper.readValue(request.getInputStream(), new TypeReference<HashMap<String, String>>() {});
+        } catch (IOException e) {
+            logger.error(e, e);
+            throw new AuthenticationServiceException(e.getMessage());
+        }
+        String username = obtainUsername(data);
+        username = (username != null) ? username.trim() : "";
+        String password = obtainPassword(data);
+        password = (password != null) ? password : "";
+        UsernamePasswordAuthenticationToken authRequest = UsernamePasswordAuthenticationToken.unauthenticated(username,
+                password);
+        // Allow subclasses to set the "details" property
+        setDetails(request, authRequest);
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+
+    protected String obtainUsername(Map<String, String> data) {
+        return data.getOrDefault(getUsernameParameter(), null);
+    }
+
+    protected String obtainPassword(Map<String, String> data) {
+        return data.getOrDefault(getPasswordParameter(), null);
+    }
+
+    @Override
+    public void setPostOnly(boolean postOnly) {
+        this.postOnly = postOnly;
     }
 
 }
