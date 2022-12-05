c3d7b64d9190ff29bed12a6c8a3b34d5c9382da8
Amir Kumalov <akumalov@integra-soft.ru>
2022-12-05 11:43:50 +0300

    Fix Configurer extends types

--- src/main/java/com/crynet/springsecuritydemo/security/CustomConfigurer.java
+++ src/main/java/com/crynet/springsecuritydemo/security/CustomConfigurer.java
@@ -2,13 +2,11 @@ package com.crynet.springsecuritydemo.security;
 
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 public final class CustomConfigurer<H extends HttpSecurityBuilder<H>> extends
-        AbstractAuthenticationFilterConfigurer<H, FormLoginConfigurer<H>, UsernamePasswordAuthenticationFilter> {
+        AbstractAuthenticationFilterConfigurer<H, CustomConfigurer<H>, CustomAuthenticationFilter> {
 
     public CustomConfigurer() {
         super(new CustomAuthenticationFilter(), null);
